5d672fbf6b7e02e0fe0f7c723dc06b74f905d050
Medido1 <medi1222@gmail.com>
2024-09-14 18:40:51 +0100

    Add a pushups counter

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const myInput = document.getElementById("chinups");
-const submitBtn = document.querySelector(".btn");
-const currentNumber = document.querySelector(".current_number");
-const progressBar = document.querySelector(".bar");
-const percentage = document.querySelector(".percentage");
-const resetBtn = document.querySelector(".reset");
+const chinupsInput = document.getElementById("chinups");
+const chinupsSubmit = document.querySelector(".btn.chinups");
+const chinupsCurrentNumber = document.querySelector(".current_number.chinups");
+const chinupsProgressBar = document.querySelector(".bar.chinups");
+const chinupsPercentage = document.querySelector(".percentage.chinups");
+const chinupsReset = document.querySelector(".reset.chinups");
 
-let totalDone = Number(localStorage.getItem("totalDone")) || 0;
-currentNumber.textContent = totalDone;
-percentage.textContent = `${totalDone}%`
-progressBar.style.width = `${totalDone}%`;
+const pushupsInput = document.getElementById("pushups");
+const pushupsSubmit = document.querySelector(".btn.pushups");
+const pushupscurrentNumber = document.querySelector(".current_number.pushups");
+const pushupsProgressBar = document.querySelector(".bar.pushups");
+const pushupsPercentage = document.querySelector(".percentage.pushups");
+const pushupsReset = document.querySelector(".reset.pushups");
+
+let chinupsTotal = Number(localStorage.getItem("chinupsTotal")) || 0;
+chinupsCurrentNumber.textContent = chinupsTotal;
+chinupsPercentage.textContent = `${chinupsTotal}%`
+chinupsProgressBar.style.width = `${chinupsTotal}%`;
+
+let pushupsTotal = Number(localStorage.getItem("pushupsTotal")) || 0;
+pushupscurrentNumber.textContent = pushupsTotal;
+pushupsPercentage.textContent = `${pushupsTotal}%`;
+pushupsProgressBar.style.width = `${pushupsTotal}%`;
+
 
 function checkSucess(value) {
   if (value >= 100) {
@@ -18,11 +31,11 @@ function checkSucess(value) {
   return false;
 }
 
-function fillProgressBar(value) {
-  let currentWidth = parseInt(progressBar.style.width) || 0;
+function fillProgressBar(value, bar) {
+  let currentWidth = parseInt(bar.style.width) || 0;
   function fillBar() {
     currentWidth += 1;
-    progressBar.style.width = `${currentWidth}%`;
+    bar.style.width = `${currentWidth}%`;
     if (currentWidth > value || currentWidth > 100){
       clearInterval(intervalId)
     }
@@ -30,34 +43,76 @@ function fillProgressBar(value) {
   const intervalId = setInterval(fillBar, 25);
 }
 
-function updateCount() {
-  if (myInput.value.trim() === ""){
+function updateCount(input, type) {
+  if (input.value.trim() === ""){
     alert("enter a number dumbass!")
     return;
   }
-  let todayInput = Number(myInput.value);
-  totalDone = totalDone + todayInput;
-  localStorage.setItem("totalDone", totalDone);
-  if (checkSucess(totalDone)){
-    percentage.textContent = `${totalDone}%`;
-    fillProgressBar(totalDone);
-    myInput.value = "";
+  if (type === "chinups") {
+    updateChinUps();
+  }
+  if (type === "pushups") {
+    updatePushUps();
+  }
+  
+}
+
+function updateChinUps() {
+  let todayInput = Number(chinupsInput.value);
+  chinupsTotal = chinupsTotal + todayInput;
+  localStorage.setItem("chinupsTotal", chinupsTotal);
+  if (checkSucess(chinupsTotal)){
+    chinupsPercentage.textContent = `${chinupsTotal}%`;
+    fillProgressBar(chinupsTotal, chinupsProgressBar);
+    chinupsInput.value = "";
     return ;
   }
-  currentNumber.textContent = totalDone;
-  percentage.textContent = `${totalDone}%`;
-  fillProgressBar(totalDone);
-  myInput.value = "";
+  chinupsCurrentNumber.textContent = chinupsTotal;
+  chinupsPercentage.textContent = `${chinupsTotal}%`;
+  fillProgressBar(chinupsTotal, chinupsProgressBar);
+  chinupsInput.value = "";
 }
 
-function resetResults() {
-  totalDone = 0;
-  localStorage.setItem("totalDone", totalDone);
-  currentNumber.textContent = totalDone;
-  percentage.textContent = `${totalDone}%`;
-  progressBar.style.width = 0;
-  myInput.value = "";
+function updatePushUps() {
+  let todayInput = Number(pushupsInput.value);
+  pushupsTotal = pushupsTotal + todayInput;
+  localStorage.setItem("pushupsTotal", pushupsTotal);
+  if (checkSucess(pushupsTotal)){
+    pushupsPercentage.textContent = `${pushupsTotal}%`;
+    fillProgressBar(pushupsTotal, pushupsProgressBar)
+    pushupsInput.value = "";
+    return;
+  }
+  pushupscurrentNumber.textContent = pushupsTotal;
+  pushupsPercentage.textContent = `${pushupsTotal}%`;
+  fillProgressBar(pushupsTotal, pushupsProgressBar);
+  pushupsInput.value = "";
 }
 
-submitBtn.addEventListener("click", updateCount);
-resetBtn.addEventListener("click", resetResults);
+function resetChinups() {
+  chinupsTotal = 0;
+  localStorage.setItem("chinupsTotal", chinupsTotal);
+  chinupsCurrentNumber.textContent = chinupsTotal;
+  chinupsPercentage.textContent = `${chinupsTotal}%`;
+  chinupsProgressBar.style.width = 0;
+  chinupsInput.value = "";
+};
+
+function resetPushups() {
+  pushupsTotal = 0;
+  localStorage.setItem("pushupsTotal", pushupsTotal);
+  pushupscurrentNumber.textContent = pushupsTotal;
+  pushupsPercentage.textContent = `${pushupsTotal}%`;
+  pushupsProgressBar.style.width = 0;
+  pushupsInput.value = "";
+}
+
+chinupsSubmit.addEventListener("click", () => {
+  updateCount(chinupsInput, "chinups");
+});
+chinupsReset.addEventListener("click", resetChinups);
+
+pushupsSubmit.addEventListener("click", () => {
+  updateCount(pushupsInput, "pushups");
+})
+pushupsReset.addEventListener("click", resetPushups);
